43c903cf94b53fc835fa71205d7a9796e010d1ee
shivambankar <78548929+shivambankar@users.noreply.github.com>
2023-06-06 19:07:49 +0530

    Update Human.java

--- StaticExample/Human.java
+++ StaticExample/Human.java
@@ -16,11 +16,3 @@ public class Human {
         Human.population += 1; //for static variable, instead of 'this' class name used.
     }
 }
-//Basically constructor means using Class Human as fuction so that we can create objects
-//out of it like calling the function(see the Main file).
-
-//Static variable is not dependent on the objects.
-//In main() method of public static void main(), static is used so that main method can be used
-//without creating objects of class.
-
-//Class inside a class is possible but outer class cant be static.
